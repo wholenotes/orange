1d41147c3034a2e6f2dbfe68bc5df1110207f32a
blazingpair <sibilant.threads@gmail.com>
2013-09-05 19:38:26 -0700

    working globe with automatically scrolling by country name

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orange::Application.config.secret_token = 'ec71b66a92c9192e8046280c58399f3d9d7eff9c7e5b393e7e29755432c499089f119baa05aa27ce4666f259556c0aade46b0ef3793964c861a7eb48ca30ac9b'
