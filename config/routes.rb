c92bb2ea64d305c377872ea37cfb973bd7f5c050
Bonnie <sibilant.threads@gmail.com>
2014-06-06 10:54:29 -0700

    clicking on country populates author content by ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Orange::Application.routes.draw do
+  resources :countries
+
+
   match 'azimuthal' => 'azimuthal#show'
+  match 'countries/:name' => 'countries#show'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
